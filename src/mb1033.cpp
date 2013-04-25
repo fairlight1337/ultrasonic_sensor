f92c3a33e3bd0ad87017dc135d560c19e5022e56
Jan Winkler <winkler@cs.uni-bremen.de>
2013-04-25 18:43:47 +0200

    Changed output value to meters (float32) instead of mm (int32). Also, added device path as parameter to node.

--- src/mb1033.cpp
+++ src/mb1033.cpp
@@ -1,5 +1,5 @@
 #include <ros/ros.h>
-#include <std_msgs/Int32.h>
+#include <std_msgs/Float32.h>
 #include <string>
 #include <sstream>
 #include <SerialStream.h>
@@ -9,19 +9,34 @@ using namespace LibSerial;
 
 
 int main(int argc, char **argv) {
+  // ROS init
   ros::init(argc, argv, "ultrasonic_sensor_mb1033");
   ros::NodeHandle nh("~");
   
-  ros::Publisher pubDistance = nh.advertise<std_msgs::Int32>("distance", 1);
+  ros::Publisher pubDistance = nh.advertise<std_msgs::Float32>("distance", 1);
   
-  string strSerialPort = "/dev/ttyUSB0";
+  // Parameters
+  string strSerialPort;
+  
+  nh.param("device", strSerialPort, string("/dev/ttyUSB0"));
+  
+  // Serial port init
   SerialPort serialPort(strSerialPort.c_str());
-  serialPort.Open(SerialPort::BAUD_9600,
+
+  try {
+    serialPort.Open(SerialPort::BAUD_9600,
 		    SerialPort::CHAR_SIZE_8,
 		    SerialPort::PARITY_NONE,
 		    SerialPort::STOP_BITS_1,
 		    SerialPort::FLOW_CONTROL_HARD);
+  } catch(...) {
+    ROS_ERROR("Failed to open device '%s'. Maybe adjust the device path by setting the 'device' parameter accordingly. Quitting.", strSerialPort.c_str());
+
+    ros::shutdown();
+    return -1;
+  }
   
+  // Start operating
   if(serialPort.IsOpen()) {
     while(ros::ok()) {
       string strLine = serialPort.ReadLine(0, '\r');
@@ -33,11 +48,11 @@ int main(int argc, char **argv) {
 	int nDistance;
 	issBuffer >> nDistance;
 	
-	if(nDistance >= 300) { // Distances are at least 300mm for this sensor
-	  std_msgs::Int32 int32Out;
-	  int32Out.data = nDistance;
+	if(nDistance >= 300 && nDistance <= 5000) { // Distances are at least 300mm and at most 5000mm for this sensor
+	  std_msgs::Float32 float32Out;
+	  float32Out.data = (float)nDistance / 1000;
 	  
-	  pubDistance.publish(int32Out);
+	  pubDistance.publish(float32Out);
 	}
       }
       
@@ -50,6 +65,7 @@ int main(int argc, char **argv) {
     return 0;
   } else {
     ROS_ERROR("Couldn't open serial terminal %s, quitting.", strSerialPort.c_str());
+    ros::shutdown();
     return -1;
   }
 }
